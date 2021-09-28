491f948db8990b2701023fa1cd099455295c3c20
kws39 <kws39@DESKTOP-EK67K8R>
2021-09-29 00:58:08 +0900

    9.28 숙제

--- ch05/src/ch04_array2/Array2Exam01.java
+++ ch05/src/ch04_array2/Array2Exam01.java
@@ -1,5 +1,9 @@
 package ch04_array2;
 
+import java.util.Arrays;
+
+import java.util.Scanner;
+
 public class Array2Exam01 {
 
 	public static void main(String[] args) {
@@ -10,6 +14,51 @@ public class Array2Exam01 {
 		//for문과 향상된 for문으로 출력하기
 		//1. 성적입력 | 2. 통계(합/평균)출력 | 3. 성적순 출력하기 | 4.종료
 		//ch04 - 7번 참조.
+		
+		Scanner scanner = new Scanner(System.in);
+		int menu = 0;
+		int [] scores = new int[8];
+		int sum =0;
+		double ave=0;
+		boolean run =true;
+		while(run) {
+			System.out.println("1. 성적입력 | 2. 통계(합/평균)출력 | 3. 성적순 출력하기 | 4.종료");
+			menu = scanner.nextInt();//메뉴버튼
+		  switch(menu) {
+		   case 1:
+		      System.out.print("성적입력(8개)> ");
+		      for(int i =0;i<scores.length;i++)
+		      {
+		    	  scores[i]=scanner.nextInt();
+		      }
+		      break;
+		   case 2:
+			   for(int s : scores)
+			   {sum+=s;}
+			   ave = sum/(double)scores.length;
+			   
+		      System.out.println("통계 합> "+sum);
+		      System.out.println("통계 평균> "+ave);
+		      
+		      break;
+		   case 3:
+		       Arrays.sort(scores);
+			      for(int i =0;i<scores.length;i++)
+			      {
+			    	  System.out.println(scores[i]);
+			      }
+		       
+		      break;
+		   case 4:
+		      run = false;
+		      System.out.println("프로그램을 종료합니다.");
+		      break;
+		   default:System.out.println("잘못 입력하셨습니다.");
+		   	
+		 }
+		}
+		System.out.println();
+		
 		
 		
 	}
